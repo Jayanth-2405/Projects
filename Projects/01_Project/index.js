3f029137b83c9bfd57809151602fa299c4dfc9f9
Jayanth-2405 <jayanthkumar1129@gmail.com>
2024-07-31 03:28:14 +0530

    background changer

--- Projects/01_Project/index.js
+++ Projects/01_Project/index.js
@@ -6,7 +6,7 @@ buttons.forEach(function (button) {
 console.log(button);
 button.addEventListener('click', function(e){
 console.log(e);
-console.log(e.target);
+console.log(e.target)
 if (e.target.id ===`gray`){
     body.style.backgroundColor=e.target.id;
 }
