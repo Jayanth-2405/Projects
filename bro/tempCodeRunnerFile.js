34c34d9b2bde0c82d9c39fa67034756aaba11742
Jayanth-2405 <jayanthkumar1129@gmail.com>
2024-08-01 08:19:59 +0530

    javascript

---
+++ bro/tempCodeRunnerFile.js
@@ -0,0 +1,15 @@
+let username;
+// document.getElementById("myBody").style.backgroundColor ="skyblue";
+
+// document.getElementById("mySubmit").onclick = function() {
+// username = document.getElementById("myText").value;
+// document.getElementById("myH1").innerText =`${username}`
+// console.log(myH1);
+// document.getElementById("myP").innerText=`${username +" your logged in user"}`
+// console.log(myP);
+// document.getElementById("myP").style.color =`magenta`
+// }
+// document.getElementById("myH1").style.color =`red`
+//  function calculate(...num1){
+//     return num1
+//  } 
